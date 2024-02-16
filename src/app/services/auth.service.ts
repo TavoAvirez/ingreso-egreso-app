497ea87f770958a6d8d039d5b9baa63c3d825b36
Tavo Avirez <gustavo.avila@gmail.com>
2024-02-16 15:39:09 -0600

    commit a base de datos

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable, Injector } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Firestore } from '@angular/fire/firestore/firestore';
+import { getDatabase, ref, set } from 'firebase/database';
 import { map, Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
 @Injectable({
@@ -9,45 +9,50 @@ import { Usuario } from '../models/usuario.model';
 export class AuthService {
 
   constructor(
-    private injector: Injector, 
+    private injector: Injector,
     private auth: AngularFireAuth,
-    
-    ) {
+  ) {
     // console.log(this.firestore.toJSON())
   }
 
   initAuthListener() {
-    this.auth.authState.subscribe( firebaseUser => {
+    this.auth.authState.subscribe(firebaseUser => {
       console.log(firebaseUser)
     });
   }
 
-  crearUsuario(nombre: string, email: string, password: string) {
-    const res = this.auth.createUserWithEmailAndPassword(email.trim(),
-      password.trim())
-        .then(({user}) => {
-          const newUser = new Usuario((user?.uid as string), nombre, email);
-          
-        });
-    return res;
+  async crearUsuario(nombre: string, email: string, password: string) {
+    const { user } = await this.auth.createUserWithEmailAndPassword(      
+      email,
+      password
+    );
+
+    const db = getDatabase();
+    await set(ref(db, `${user?.uid}/usuario`), {
+      uid: user?.uid,
+      nombre,
+      email,
+    });
+
+    return user;
   }
 
-  loginUsuario(email: string, password:string) {
+  loginUsuario(email: string, password: string) {
     const res = this.auth.signInWithEmailAndPassword(email.trim(), password.trim());
     return res;
   }
 
-  logout(){
+  logout() {
     return this.auth.signOut();
   }
 
   isAuth(): Observable<boolean> {
     return this.auth.authState.pipe(
       map(firebaseUser => {
-        return firebaseUser != null 
+        return firebaseUser != null
       })
     );
 
-    
+
   }
 }
